701a21adbd2cc03c49184ed3bda38c4a1bf7fb49
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-06-29 21:39:28 -0400

    working on writing a ModelFactory method for the Sentinel User

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,21 +1,10 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
-
-$factory->define(App\User::class, function ($faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->email,
-        'password' => str_random(10),
-        'remember_token' => str_random(10),
-    ];
+$factory->define(Sentinel::class, function($faker) {
+  return [
+    'first_name' => $faker->first_name,
+    'last_name' => $faker->last_name,
+    'email' => $faker->email,
+    'password' => $faker->password
+  ];
 });
---
+++ resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Laravel with Sentinel</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+<body>
+  <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
